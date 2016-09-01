348bf479fcf9ca64a9195bbe78c25e5dacd616e7
Scott Cote <cote.scott.f@gmail.com>
2016-09-01 03:07:04 -0400

    Asset proxy complete

--- dist/app.js
+++ dist/app.js
@@ -16,36 +16,64 @@ var executeDbRequest = function(request, response) {
   });
 };
 
+var buildRequestHeaders = function(headers) {
+	var newHeaders = {};
+	Object.keys(headers).forEach((key) => {
+		if (!['host','connection','referer'].find(element => key === element)) {
+			newHeaders[key] = headers[key];
+		}
+	});
+	newHeaders.host = config.host;
+	if (headers.referer) {
+		var protocol = config.secureHost ? 'https://' : 'http://';
+		newHeaders.referer = protocol+config.host+url.parse(headers.referer).path;
+	}
+	return newHeaders;
+};
+
+var buildResponseHeaders = function(headers) {
+	var newHeaders = {};
+	Object.keys(headers).forEach((key) => {
+		if (![].find(element => key === element)) {
+			newHeaders[key] = headers[key];
+		}
+	});
+	return newHeaders;
+};
+
 var proxyAssetRequest = function(request, response) {
   return new Promise((resolve, reject) => {
     var protocol = config.secureHost ? https : http;
-    var assetProtocol = config.secureHost ? 'https://' : 'http://';
-    var assetUrl = assetProtocol+config.host+url.parse(request.url).path;
-    protocol.get(assetUrl, (res) => {
-      //console.log('statusCode:', res.statusCode);
-      //console.log('headers:', res.headers);
+    protocol.request({
+      host: config.host,
+      path: url.parse(request.url).path,
+      headers: buildRequestHeaders(request.headers)
+    }, (res) => {
+      response.statusCode = res.statusCode;
+      var responseHeaders = buildResponseHeaders(res.headers);
+      Object.keys(responseHeaders).forEach((key) => {
+        response.setHeader(key, responseHeaders[key]);
+      });
       res.on('end', resolve).on('error', reject).pipe(response);
-    }).on('error', reject);
+    }).on('error', reject).end();
   });
+};
+
+var handleRequest = function(request, response) {
+  if (isDbRequest(request.url)) {
+    executeDbRequest(request, response)
+      .catch((e) => console.log('Execute DB error: '+e));
+  } else {
+    proxyAssetRequest(request, response)
+      .catch((e) => console.log('Proxy asset error: '+e));
+  }
 };
 
 (function() {
   var homePath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
   var configPath = path.join(homePath, '.plsqlrunner', 'config.json');
   config = require(configPath);
-
-  var handler = function(request, response) {
-    if (isDbRequest(request.url)) {
-      executeDbRequest(request, response)
-        .catch((e) => console.log('Execute DB error: '+e));
-    } else {
-      proxyAssetRequest(request, response)
-        .catch((e) => console.log('Proxy asset error: '+e));
-    }
-  };
-
-  http.createServer(handler).listen(5150, function() {
+  http.createServer(handleRequest).listen(5150, function() {
     console.log("PLSQLRunner listening on: http://localhost:5150");
   });
-
 })();
