e9fd1298d51fd605ebc9c6de84a7345a9cf8c3d0
Scott Cote <cote.scott.f@gmail.com>
2016-08-31 21:23:00 -0400

    Removed dependency of Koa

--- dist/app.js
+++ dist/app.js
@@ -1,47 +1,51 @@
-var koa = require('koa');
 var path = require('path');
 var url = require('url');
 var http = require('http');
 var https = require('https');
-var app = koa();
-
-var runApp = function() {
-  var homePath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-  var configPath = path.join(homePath, '.plsqlrunner', 'config.json');
-  config = require(configPath);
-  app.listen(5150);
-};
 
 var isDbRequest = function(urlString) {
   return url.parse(urlString).pathname.startsWith(config.basePath);
 };
 
-var executeDbRequest = function() {
-  var packagePath = url.parse(this.url).pathname.replace(config.basePath, '').split('.');
-  if (packagePath.length < 3) packagePath.unshift(config.defaultSchema);
-  this.body = 'executeDbRequest not implemented for '+packagePath.join('.');
+var executeDbRequest = function(request, response) {
+  return new Promise((resolve, reject) => {
+    var packagePath = url.parse(request.url).pathname.replace(config.basePath, '').split('.');
+    if (packagePath.length < 3) packagePath.unshift(config.defaultSchema);
+    response.end('executeDbRequest not implemented for '+packagePath.join('.'));
+    resolve();
+  });
 };
 
-var proxyAssetRequest = function() {
+var proxyAssetRequest = function(request, response) {
   return new Promise((resolve, reject) => {
     var protocol = config.secureHost ? https : http;
     var assetProtocol = config.secureHost ? 'https://' : 'http://';
-    var assetUrl = assetProtocol+config.host+url.parse(this.url).path;
+    var assetUrl = assetProtocol+config.host+url.parse(request.url).path;
     protocol.get(assetUrl, (res) => {
       //console.log('statusCode:', res.statusCode);
       //console.log('headers:', res.headers);
-      res.pipe(this.res);
-      res.on('end', resolve);
+      res.on('end', resolve).on('error', reject).pipe(response);
     }).on('error', reject);
   });
 };
 
-app.use(function *() {
-  if (isDbRequest(this.url)) {
-    return executeDbRequest.call(this);
-  } else {
-    return proxyAssetRequest.call(this);
-  }
-});
+(function() {
+  var homePath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+  var configPath = path.join(homePath, '.plsqlrunner', 'config.json');
+  config = require(configPath);
 
-runApp();
+  var handler = function(request, response) {
+    if (isDbRequest(request.url)) {
+      executeDbRequest(request, response)
+        .catch((e) => console.log('Execute DB error: '+e));
+    } else {
+      proxyAssetRequest(request, response)
+        .catch((e) => console.log('Proxy asset error: '+e));
+    }
+  };
+
+  http.createServer(handler).listen(5150, function() {
+    console.log("PLSQLRunner listening on: http://localhost:5150");
+  });
+
+})();
